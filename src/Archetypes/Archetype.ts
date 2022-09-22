4b74935acebf826ef3dad3dcda7c4d0c6c930dd8
ThalysRD <tgbr66@gmail.com>
2022-09-22 17:10:21 -0300

    Cria a classe Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,25 @@
+abstract class Archetype {
+  private _name : string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string, special = 0, cost = 0) {
+    this._name = name;
+    this._special = special;
+    this._cost = cost;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+}
+
+export default Archetype;
